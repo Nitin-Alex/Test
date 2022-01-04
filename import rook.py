eb3d0ea1291b743de4ab7af394954956f19c76d8
Nitin Alex <22214550+Nitin-Alex@users.noreply.github.com>
2022-01-04 11:45:41 +0530

    Update import rook.py

--- import rook.py
+++ import rook.py
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-=======
-import rook
-
-if __name__ == "__main__":
-    rook.start(token='ee463e7de481d1e67d230ada4995cb42fee2dc178097203485f51dd8b9603c6f',
-               labels={"env":"dev"}) # Optional,see Labels page below Projects
-    # Your program starts here :)
->>>>>>> eed1547919e74e7c9476060dc0b2b4e696bebe9b
 print("Hi")
